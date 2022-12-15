42ddfe817b3c61c34c4210439f4349553417d3d2
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-15 22:19:01 +0300

    050

--- homework050/Program.cs
+++ homework050/Program.cs
@@ -9,3 +9,80 @@
 17 -> такого числа в массиве нет
 */
 
+// получить числа с консоли
+int GetNumber(string message)
+{
+   int result = 0;
+   while (true)
+   {
+      Console.WriteLine(message);
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         break;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число");
+      }
+   }
+   return result;
+}
+
+// задать матрицу и заполнить ее числами
+int[,] GetMatrix(int m, int n)
+{
+   int[,] matrix = new int[m, n];
+   Random rnd = new Random();
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         matrix[i, j] = rnd.Next(1, 10);
+      }
+   }
+   return matrix;
+}
+
+// распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         Console.Write($"{matrix[i, j]} ");
+      }
+      Console.WriteLine();
+   }
+}
+
+void Poisk(int[,] matrix)
+{
+   int x = GetNumber("Введите номер строки элемента");
+   int y = GetNumber("Введите номер столбца элемента");
+   int num = 0;
+   string result = $"такого элемента нет";
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         if (i == x && j == y)
+         {
+            num = matrix[x - 1, y - 1];
+            result = $"Искомый элемент = {num}";
+         }
+         else if (x < 0 || y < 0 || x > matrix.GetLength(0) || y > matrix.GetLength(1))
+         {
+            break;
+         }
+      }
+   }
+   Console.WriteLine(result);
+}
+
+int m = GetNumber("Введите количество строк");
+int n = GetNumber("Введите количество столбцов");
+
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+Poisk(matrix);
